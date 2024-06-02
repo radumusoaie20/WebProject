4a4e5e14026533ef5d0700859fc3dfaba4907119
AndreeaGutuiC <162283451+AndreeaGutuiC@users.noreply.github.com>
2024-06-02 12:52:42 +0300

    Fetch and count symptom occurrences from Firebase Database

--- scripts/results.js
+++ scripts/results.js
@@ -1,4 +1,4 @@
-import { getDatabase, ref, onValue} from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js"
+import { get, getDatabase, ref } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js";
 // Get the diseases from the URL
 let urlParams = new URLSearchParams(window.location.search);
 let symptoms = (urlParams.get('symptoms')).split(',');
@@ -13,48 +13,26 @@ symptoms
     ->"anxiety" : [LIST OF DISEASES],
       "anorexia" : [LIST OF DISEASES] 
 */
-let dict = [];
-document.addEventListener('DOMContentLoaded', function(){
-    fetchDiseases();
-    setTimeout(execAfterFetch, 4000); //should be replaced with a promise, we need to wait for the data to be given
-}
-);
-function execAfterFetch(){
-    const div = document.querySelector('#results');
-    let arr = Object.keys(dict).map((e)=>{return [e, dict[e]];});
-    arr.sort((a, b)=>{return (b[1]-a[1])});
-    for(let i = 0; i < arr.length; i++){
-        const p = document.createElement('p');
-        p.innerHTML = arr[i][0];
-        div.appendChild(p);
-    }
-}
 
-function fetchDiseases(){
+let sym = [];
+function fetchSymptomsDiseases(){
     const db = getDatabase();
-    for(const symptom of symptoms){
-        fetchDiseasesForSymptom(db, symptom);
-    }
+    const dbRef = ref(db, '/symptoms');
+    get(dbRef).then((snapshot)=>{
+        const data = snapshot.val();
+        sym = data;
+    })
 }
-
-//Implement using promises
-function getDiseaseSymptomCount(disease){
-    const db = getDatabase();
-    const dbRef = ref(db, `diseases/${disease}/symptoms`);
-    onValue(dbRef, (data)=>{
-        let parseData = data.val();
-        return parseData.length;
-    });
-}
-function fetchDiseasesForSymptom(db, symptom){
-    const dbRef = ref(db, `symptoms/${symptom}`);
-        onValue(dbRef, (data)=>{
-            let parseData = data.val();
-            if(data !== null){
-                for(const disease of parseData){
-                  if(disease in dict) dict[disease]++;
-                  else dict[disease] = 1;
-            }
+function waitForAssign(){
+    setTimeout(()=>{
+        if(sym === undefined){
+            waitForAssign();
         }
-    });
+        return;
+    }, 1000);
 }
+function loadSymptoms(){
+    fetchSymptomsDiseases();
+    waitForAssign();
+}
+document.addEventListener('DOMContentLoaded', waitForAssign);
